82d571668fc422238bd7f8c3f8df686810fe13ea
unknown <rasik.hinge4@gmail.com>
2019-08-04 15:52:39 +0530

    updated

---
+++ src/main/java/com/reactive/FluxPractice.java
@@ -0,0 +1,16 @@
+package com.reactive;
+
+import reactor.core.publisher.Flux;
+
+public class FluxPractice {
+
+    public static void main(String[] args) {
+        System.out.println("Flux practice..");
+//
+//        Flux<Integer> range = Flux.range(4, 5);
+////        range.map(i -> i * 2).subscribe(System.out::println);
+//        range.flatMap(i -> {
+//            return i.toString();
+//        });
+    }
+}
